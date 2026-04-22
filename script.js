ab59783cde755a4975955443cafb4c38b4270690
richard <richardpsdsilva@gmail.com>
2026-04-22 16:11:54 -0300

    atividade

--- script.js
+++ script.js
@@ -1,83 +1,90 @@
-// 1 .Classe Cliente
+// 1.  Classe Cliente :
 
 class Cliente {
 
-    // Metodo Construtor : Excelente quando criamos um novo cliente
+  // Método construtor: executado quando criamos um novo cliente
+  constructor(nome, cpf, agencia, conta) {
 
-    constructor(nome, cpf, agencia, conta) {
-        // "this" representa o objeto que esta sendo criado 
-        this.nome = nome; // Armazena o nome recebido
-        this.cpf = cpf;
-        this.agencia = agencia;
-        this.conta = conta;
-
-    }
-
+    // "this" representa o objeto que está sendo criado
+    this.nome = nome; // Armazena o nome recebido
+    this.cpf = cpf;
+    this.agencia = agencia;
+    this.conta = conta;
+  }
 }
 
-// 2 . Classe Conta Corrente:
+// 2. Classe ContaCorrente:
 
 class ContaCorrente {
+  // Construtor recebe um cliente já criado
+  constructor(cliente) {
+    this.cliente = cliente; // Associa a conta a um cliente
+    this.saldo = 0; // Inicia o saldo com 0
+  }
 
-    // Construtor recebe um cliente ja criado
+  //2.1 Método para realizar depósito
+  depositar(valor) {
+    // Guarda o saldo antes da operação
+    let saldoAnterior = this.saldo;
 
-    constructor(cliente) {
+    // Soma o valor depositado ao saldo atual
+    this.saldo += valor;
 
-        this.cliente = cliente // Associa a conta a um cliente
-        this.saldo = 0; // inicia o saldo com 0 
-
-    }
-
-
-
-    // 2.1 Metodo para realizar o deposito
-
-    depositar(valor) {
-
-        // Guarda o saldo antes da operação
-        let saldoAnterior = this.saldo;
-
-        // Soma o Valor depositado ao saldo atual
-        this.saldo += valor;
-
-        return {
-
-            valor: valor, // Saldo antes da Operação
-            saldoAnterior: saldoAnterior; // saldo antes da operação 
-            saldoAtual: this.saldo, // Novo saldo apos o deposito 
-            tipo: "Deposito", // Tipo da operação
-            data: new Date().toLocaleDateString(), //Data atual formatada
-        };
-
-    }
-
+    // Retorna um "resumo" da operação (objeto)
+    return {
+      valor: valor, // Valor depositado
+      saldoAnterior: saldoAnterior, // Saldo antes da operação
+      saldoAtual: this.saldo, // Novo saldo após depósito
+      tipo: "Depósito", // Tipo da operação
+      data: new Date().toLocaleDateString(), // Data atual formatada
+    };
+  }
 }
 
-// 3 . Funcão Principall (excultada ao clicar no botão)
-function CriarConta() {
-    // pega o valor digitado no input "nome"
-    let nome = document.getElementById("nome").Value;
-    let cpf = document.getElementById("cpf").Value;
-    let agencia = document.getElementById("agencia").Value;
-    let conta = document.getElementById("conta").Value;
+// 3. Função principal (executada ao clicar no botão)
+function criarConta() {
+  // Pega o valor digitado no input "nome" , assim para os demais
+  let nome = document.getElementById("nome").value;
+  let cpf = document.getElementById("cpf").value;
+  let agencia = document.getElementById("agencia").value;
+  let conta = document.getElementById("conta").value;
 
-    //converte o valor digitado para numero (float)
-    let valor = parseFloat(document.getElementById("valor").value);
+  // Converte o valor digitado para número (float)
+  let valor = parseFloat(document.getElementById("valor").value);
+
+  // 4. Criando objetos (instâncias das classes) :
+
+  // Cria um objeto cliente com os dados digitados
+  let cliente = new Cliente(nome, cpf, agencia, conta);
+
+  // Cria uma conta vinculada ao cliente
+  let contaCorrente = new ContaCorrente(cliente);
+
+  // Executa o depósito e guarda o resultado
+  let operacao = contaCorrente.depositar(valor);
+
+  // 5.  Exibe o extrato na tela (dentro da div "extrato") :
+
+  document.getElementById("extrato").innerText = `
+
+  --------Dados do Cliente------
+  Nome: ${cliente.nome}
+  CPF: ${cliente.cpf}
+  Agência: ${cliente.agencia}
+  Conta: ${cliente.conta}
 
 
-    // 4 . Criando o objeto cliente com os dados digitados 
-    let cliente = new Cliente(nome, cpf, agencia, conta);
+ --------Conta Corrente ----
 
-    //Cria uma conta vinculada ao cliente
+ Tipo: ${operacao.tipo}
+ Valor: ${operacao.valor}
+ Data: ${operacao.data}
+ Saldo anterior: ${operacao.saldoAnterior}
+ Saldo atual : ${operacao.saldoAtual}
 
-    let ContaCorrente = new ContaCorrente(cliente);
+  Mensagem: Operação realizada com sucesso !
 
-
-    // Executa o deposito e guarda o resultado
-    let operacao = ContaCorrente, depositar(valor);
-
-    //5 . Exibe o extrato na tela (dentro da div "estração")
-    document.getElementById("estrato")
+  `;
 
 
 }
